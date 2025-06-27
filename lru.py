c4d2cb70b3b1dc8de3faaf34fdcdd7e13756317e
BTR <thilakraj1301@gmail.com>
2025-06-26 20:07:55 -0700

    Create lru.py

---
+++ lru.py
@@ -0,0 +1,24 @@
+# Time Complexity: O(1) for both get and put operations.
+# Space Complexity: O(capacity), where capacity is the size of the cache.
+
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            value = self.cache.pop(key)
+            self.cache[key] = value
+            return value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        elif len(self.cache) >= self.capacity:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
+
